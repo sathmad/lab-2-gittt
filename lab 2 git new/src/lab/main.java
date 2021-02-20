acb7c06fa9c32de3743db4ba47a8bb2a186c74fc
sathmad <sathmad@gmail.com>
2021-02-20 19:12:54 +0530

    sathma

---
+++ lab 2 git new/src/lab/main.java
@@ -0,0 +1,11 @@
+package lab;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first project at SLIIT");
+
+	}
+
+}
